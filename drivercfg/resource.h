0efe1fffcd37230fd8b0d4a00933408c1f2243a9
Falcosoft <zbacsko@gmail.com>
2023-07-21 22:17:32 +0200

    drivercfg\resource.h cleaning up

--- drivercfg/resource.h
+++ drivercfg/resource.h
@@ -2,42 +2,25 @@
 // Microsoft Visual C++ generated include file.
 // Used by drivercfg.rc
 //
-#define VERSION_PATCH                   0
-#define VERSION_MAJOR                   1
-#define VERSION_BUILD                   1
-#define VERSION_MINOR                   5
 #define IDD_ADVANCED                    102
-#define IDD_ABOUTBOX                    103
 #define IDD_MAIN                        103
 #define IDI_DRIVERCFG                   107
 #define IDI_SMALL                       108
 #define IDR_MAINFRAME                   128
 #define IDD_MAINDLG                     130
 #define IDD_SOUND                       131
-#define IDC_SFLIST                      1000
-#define IDC_ADDSF                       1001
-#define IDC_RMSF                        1002
 #define IDC_SYNTHLIST                   1003
 #define IDC_SFAPPLY                     1004
 #define IDC_SNAPPLY                     1005
-#define IDC_SFUP                        1006
-#define IDC_UPSF                        1006
-#define IDC_DOWNSF                      1007
-#define IDC_VOICES                      1008
-#define IDC_VOICES2                     1009
-#define IDC_TAB1                        1009
-#define IDC_TAB                         1009
-#define IDC_CPUUSAGE                    1009
+#define IDC_TAB                         1008
 #define IDC_SHOWVST                     1009
 #define IDC_VSTLOADED                   1010
-#define IDC_SHOWVST2                    1011
 #define IDC_USE4CH                      1011
 #define IDC_VSTPRODUCT                  1013
 #define IDC_VSTLOAD                     1014
 #define IDC_VENDOR                      1015
 #define IDC_EFFECT                      1016
 #define IDC_PRODUCT                     1017
-#define IDC_BUTTON1                     1018
 #define IDC_VSTCONFIG                   1018
 #define IDC_LIST1                       1019
 #define IDC_COMBO1                      1020
@@ -48,7 +31,6 @@
 #define IDC_FILEVERSION                 1025
 #define IDC_VOLUME                      1027
 #define IDC_ASIOCTRLP                   1028
-#define IDC_COMBO2                      1029
 #define IDC_COMBO_PORTB                 1029
 #define IDC_STATIC_PORTB                1030
 #define IDC_ALWAYSONTOP                 1031
